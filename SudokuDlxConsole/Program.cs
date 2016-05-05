30976bf67e50fc71df34c8d43ee9b0c57d9aa97d
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-05-05 18:49:13 +0100

    Added code to verify solutions

--- SudokuDlxConsole/Program.cs
+++ SudokuDlxConsole/Program.cs
@@ -11,7 +11,7 @@ namespace SudokuDlxConsole
         private static void Main()
         {
             // http://puzzles.telegraph.co.uk/site/search_puzzle_number?id=27744
-            var gridPuzzle = new Grid(ImmutableList.Create(
+            var grid = new Grid(ImmutableList.Create(
                 "6 4 9 7 3",
                 "  3    6 ",
                 "       18",
@@ -22,39 +22,43 @@ namespace SudokuDlxConsole
                 " 4    8  ",
                 "9 8 6 4 5"));
 
-            var gridSolution = Solve(gridPuzzle);
+            grid.Draw();
 
-            Console.WriteLine("Puzzle:");
-            gridPuzzle.Draw();
+            var internalRows = BuildInternalRowsForGrid(grid);
+            var dlxRows = BuildDlxRows(internalRows);
+            var solutions = new Dlx()
+                .Solve(dlxRows, d => d, r => r)
+                .Where(solution => VerifySolution(internalRows, solution))
+                .ToImmutableList();
 
             Console.WriteLine();
 
-            Console.WriteLine("Solution:");
-            gridSolution.Draw();
+            if (solutions.Any())
+            {
+                Console.WriteLine($"Solution (1 of {solutions.Count}):");
+                Console.WriteLine();
+                DrawSolution(internalRows, solutions.First());
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("No solutions found!");
+            }
         }
 
-        private static Grid Solve(Grid grid)
-        {
-            var internalRows = BuildInternalRowsForGrid(grid);
-            var dlxRows = BuildDlxRows(internalRows);
-
-            var dlx = new Dlx();
-            var solution = dlx.Solve(dlxRows, d => d, r => r).First();
-
-            var rcvs = solution.RowIndexes.Select(rowIndex => internalRows[rowIndex]);
-            var sortedRcvs = rcvs.OrderBy(t => t.Item1).ThenBy(t => t.Item2).ToList();
-
-            var rowStrings = Enumerable.Range(0, 9)
-                .Select(row => string.Join("", sortedRcvs.Skip(row*9).Take(9).Select(t => t.Item3)));
-
-            return new Grid(rowStrings.ToImmutableList());
-        }
+        private static IEnumerable<int> Rows => Enumerable.Range(0, 9);
+        private static IEnumerable<int> Cols => Enumerable.Range(0, 9);
+        private static IEnumerable<Tuple<int, int>> Locations =>
+            from row in Rows
+            from col in Cols
+            select Tuple.Create(row, col);
+        private static IEnumerable<int> Digits => Enumerable.Range(1, 9);
 
         private static IImmutableList<Tuple<int, int, int, bool>> BuildInternalRowsForGrid(Grid grid)
         {
             var rowsByCols =
-                from row in Enumerable.Range(0, 9)
-                from col in Enumerable.Range(0, 9)
+                from row in Rows
+                from col in Cols
                 let value = grid.ValueAt(row, col)
                 select BuildInternalRowsForCell(row, col, value);
 
@@ -66,10 +70,11 @@ namespace SudokuDlxConsole
             if (value >= 1 && value <= 9)
                 return ImmutableList.Create(Tuple.Create(row, col, value, true));
 
-            return Enumerable.Range(1, 9).Select(v => Tuple.Create(row, col, v, false)).ToImmutableList();
+            return Digits.Select(v => Tuple.Create(row, col, v, false)).ToImmutableList();
         }
 
-        private static IImmutableList<IImmutableList<int>> BuildDlxRows(IEnumerable<Tuple<int, int, int, bool>> internalRows)
+        private static IImmutableList<IImmutableList<int>> BuildDlxRows(
+            IEnumerable<Tuple<int, int, int, bool>> internalRows)
         {
             return internalRows.Select(BuildDlxRow).ToImmutableList();
         }
@@ -91,14 +96,92 @@ namespace SudokuDlxConsole
 
         private static int RowColToBox(int row, int col)
         {
-            return row - (row % 3) + (col / 3);
+            return row - (row%3) + (col/3);
         }
 
         private static IEnumerable<int> Encode(int major, int minor)
         {
             var result = new int[81];
-            result[major * 9 + minor] = 1;
+            result[major*9 + minor] = 1;
             return result.ToImmutableList();
+        }
+
+        private static bool VerifySolution(IReadOnlyList<Tuple<int, int, int, bool>> internalRows, Solution solution)
+        {
+            var solutionInternalRows = solution.RowIndexes
+                .Select(rowIndex => internalRows[rowIndex])
+                .ToImmutableList();
+
+            var locationsGroupedByRow = Locations.GroupBy(t => t.Item1);
+            var locationsGroupedByCol = Locations.GroupBy(t => t.Item2);
+            var locationsGroupedByBox = Locations.GroupBy(t => RowColToBox(t.Item1, t.Item2));
+
+            return
+                CheckGroupsOfLocations(solutionInternalRows, locationsGroupedByRow, "row") &&
+                CheckGroupsOfLocations(solutionInternalRows, locationsGroupedByCol, "col") &&
+                CheckGroupsOfLocations(solutionInternalRows, locationsGroupedByBox, "box");
+        }
+
+        private static bool CheckGroupsOfLocations(
+            IEnumerable<Tuple<int, int, int, bool>> solutionInternalRows,
+            IEnumerable<IGrouping<int, Tuple<int, int>>> groupedLocations,
+            string tag)
+        {
+            return groupedLocations.All(grouping =>
+                CheckLocations(solutionInternalRows, grouping, grouping.Key, tag));
+        }
+
+        private static bool CheckLocations(
+            IEnumerable<Tuple<int, int, int, bool>> solutionInternalRows,
+            IEnumerable<Tuple<int, int>> locations,
+            int key,
+            string tag)
+        {
+            var digits = locations.SelectMany(location =>
+                solutionInternalRows
+                    .Where(solutionInternalRow =>
+                        solutionInternalRow.Item1 == location.Item1 &&
+                        solutionInternalRow.Item2 == location.Item2)
+                    .Select(t => t.Item3));
+            return CheckDigits(digits, key, tag);
+        }
+
+        private static bool CheckDigits(
+            IEnumerable<int> digits,
+            int key,
+            string tag)
+        {
+            var actual = digits.OrderBy(v => v);
+            if (actual.SequenceEqual(Digits)) return true;
+            var values = string.Concat(actual.Select(n => Convert.ToString(n)));
+            Console.WriteLine($"{tag} {key}: {values} !!!");
+            return false;
+        }
+
+        private static Grid SolutionToGrid(
+            IReadOnlyList<Tuple<int, int, int, bool>> internalRows,
+            Solution solution)
+        {
+            var solutionInternalRows = solution.RowIndexes
+                .Select(rowIndex => internalRows[rowIndex])
+                .OrderBy(t => t.Item1)
+                .ThenBy(t => t.Item2);
+
+            var rowStrings = Rows.Select(row =>
+                string.Concat(solutionInternalRows
+                    .Skip(row*9)
+                    .Take(9)
+                    .Select(t => t.Item3)));
+
+            return new Grid(rowStrings.ToImmutableList());
+        }
+
+        private static void DrawSolution(
+            IReadOnlyList<Tuple<int, int, int, bool>> internalRows,
+            Solution solution)
+        {
+            var grid = SolutionToGrid(internalRows, solution);
+            grid.Draw();
         }
     }
 }
