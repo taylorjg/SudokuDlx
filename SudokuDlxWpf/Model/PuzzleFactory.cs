2c895a7c0166e1973f20222fca8f4b57cc6619a7
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-05-07 18:41:02 +0100

    Can now read a puzzle in from a JSON resource

---
+++ SudokuDlxWpf/Model/PuzzleFactory.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace SudokuDlxWpf.Model
+{
+    public static class PuzzleFactory
+    {
+        public static Puzzle CreatePuzzleFromJsonResource(string resourceName)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            using (var stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                if (stream == null) throw new ApplicationException($"Failed to load resource {resourceName}");
+                var jsonSerializer = new JsonSerializer();
+                using (var streamReader = new StreamReader(stream))
+                using (var jsonTextReader = new JsonTextReader(streamReader))
+                {
+                    var puzzleData = jsonSerializer.Deserialize<dynamic>(jsonTextReader);
+                    var rowStrings = (JArray)puzzleData["rowStrings"];
+                    var initialValues = RowStringsToInitialValues(rowStrings);
+                    return new Puzzle(initialValues);
+                }
+            }
+        }
+
+        private static IImmutableList<InitialValue> RowStringsToInitialValues(JArray rowStrings)
+        {
+            return rowStrings.SelectMany(RowStringToInitialValues).ToImmutableList();
+        }
+
+        private static IEnumerable<InitialValue> RowStringToInitialValues(JToken rowString, int row)
+        {
+            var indices = Enumerable.Range(0, int.MaxValue);
+            return ((string) rowString)
+                .Zip(indices, Tuple.Create)
+                .Where(t => char.IsDigit(t.Item1))
+                .Select(t => new InitialValue(new Coords(row, t.Item2), t.Item1 - '0'));
+        }
+    }
+}
--- SudokuDlxWpf/View/MainWindow.xaml.cs
+++ SudokuDlxWpf/View/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Immutable;
-using SudokuDlxWpf.Model;
+﻿using SudokuDlxWpf.Model;
 using SudokuDlxWpf.ViewModel;
 
 namespace SudokuDlxWpf.View
@@ -12,38 +11,11 @@ namespace SudokuDlxWpf.View
 
             ContentRendered += (_, __) =>
             {
+                var puzzle = PuzzleFactory.CreatePuzzleFromJsonResource("SudokuDlxWpf.SamplePuzzles.DailyTelegraph27744.json");
                 IBoardControl bc = BoardControl;
                 bc.Initialise();
                 bc.Reset();
-                var initialValues = new[]
-                {
-                    new InitialValue(new Coords(0, 0), 6),
-                    new InitialValue(new Coords(0, 2), 4),
-                    new InitialValue(new Coords(0, 4), 9),
-                    new InitialValue(new Coords(0, 6), 7),
-                    new InitialValue(new Coords(0, 8), 3),
-                    new InitialValue(new Coords(1, 2), 3),
-                    new InitialValue(new Coords(1, 7), 6),
-                    new InitialValue(new Coords(2, 7), 1),
-                    new InitialValue(new Coords(2, 8), 8),
-                    new InitialValue(new Coords(3, 3), 1),
-                    new InitialValue(new Coords(3, 4), 8),
-                    new InitialValue(new Coords(3, 8), 9),
-                    new InitialValue(new Coords(4, 5), 4),
-                    new InitialValue(new Coords(4, 6), 3),
-                    new InitialValue(new Coords(5, 0), 7),
-                    new InitialValue(new Coords(5, 4), 3),
-                    new InitialValue(new Coords(5, 5), 9),
-                    new InitialValue(new Coords(6, 1), 7),
-                    new InitialValue(new Coords(7, 1), 4),
-                    new InitialValue(new Coords(7, 6), 8),
-                    new InitialValue(new Coords(8, 0), 9),
-                    new InitialValue(new Coords(8, 2), 8),
-                    new InitialValue(new Coords(8, 4), 6),
-                    new InitialValue(new Coords(8, 6), 4),
-                    new InitialValue(new Coords(8, 8), 5)
-                }.ToImmutableList();
-                bc.AddInitialValues(initialValues);
+                bc.AddInitialValues(puzzle.InitialValues);
             };
         }
     }
