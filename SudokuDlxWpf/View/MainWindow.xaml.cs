7d0ba85defbfb69953fed6ea56d5f0b9eb02bc0a
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-05-07 19:43:33 +0100

    Now displaying the solution

--- SudokuDlxWpf/View/MainWindow.xaml.cs
+++ SudokuDlxWpf/View/MainWindow.xaml.cs
@@ -1,10 +1,15 @@
-﻿using SudokuDlxWpf.Model;
+﻿using System.Collections.Immutable;
+using System.Threading;
+using SudokuDlxWpf.Model;
 using SudokuDlxWpf.ViewModel;
 
 namespace SudokuDlxWpf.View
 {
     public partial class MainWindow
     {
+        private CancellationTokenSource _cancellationTokenSource;
+        private PuzzleSolver _puzzleSolver;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -14,9 +19,37 @@ namespace SudokuDlxWpf.View
                 var puzzle = PuzzleFactory.CreatePuzzleFromJsonResource("SudokuDlxWpf.SamplePuzzles.DailyTelegraph27744.json");
                 IBoardControl bc = BoardControl;
                 bc.Initialise();
-                bc.Reset();
                 bc.AddInitialValues(puzzle.InitialValues);
+
+                _cancellationTokenSource = new CancellationTokenSource();
+
+                _puzzleSolver = new PuzzleSolver(
+                    puzzle,
+                    OnSolutionFound,
+                    OnSearchStep,
+                    SynchronizationContext.Current,
+                    _cancellationTokenSource.Token);
+
+                _puzzleSolver.SolvePuzzle();
             };
+
+            Closed += (_, __) => _cancellationTokenSource?.Cancel();
+        }
+
+        private void OnSearchStep(IImmutableList<InternalRow> searchStepInternalRows)
+        {
+        }
+
+        private void OnSolutionFound(IImmutableList<InternalRow> solutionInternalRows)
+        {
+            foreach (var x in solutionInternalRows)
+            {
+                IBoardControl bc = BoardControl;
+                if (!x.IsInitialValue)
+                {
+                    bc.AddDigit(x.Coords, x.Value);
+                }
+            }
         }
     }
 }
