440a4d35554999e58b22ed70b1ec6cdcae980b91
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-05-07 21:41:13 +0100

    Now visualising the search steps

--- SudokuDlxWpf/View/MainWindow.xaml.cs
+++ SudokuDlxWpf/View/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System.Collections.Immutable;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
 using System.Threading;
+using System.Windows.Threading;
 using SudokuDlxWpf.Model;
 using SudokuDlxWpf.ViewModel;
 
@@ -8,47 +12,123 @@ namespace SudokuDlxWpf.View
     public partial class MainWindow
     {
         private CancellationTokenSource _cancellationTokenSource;
-        private PuzzleSolver _puzzleSolver;
+        private IBoardControl _bc;
+        private readonly List<InternalRow> _currentInternalsRows = new List<InternalRow>();
+        private readonly DispatcherTimer _timer = new DispatcherTimer();
+        private readonly Queue<IImmutableList<InternalRow>> _searchSteps = new Queue<IImmutableList<InternalRow>>();
 
         public MainWindow()
         {
             InitializeComponent();
 
+            _timer.Tick += (_, __) => OnTick();
+            _timer.Interval = TimeSpan.FromMilliseconds(100);
+
             ContentRendered += (_, __) =>
             {
                 var puzzle = PuzzleFactory.CreatePuzzleFromJsonResource("SudokuDlxWpf.SamplePuzzles.DailyTelegraph27744.json");
-                IBoardControl bc = BoardControl;
-                bc.Initialise();
-                bc.AddInitialValues(puzzle.InitialValues);
+
+                _bc = BoardControl;
+                _bc.Initialise();
+                _bc.AddInitialValues(puzzle.InitialValues);
 
                 _cancellationTokenSource = new CancellationTokenSource();
+                _currentInternalsRows.Clear();
+                _searchSteps.Clear();
 
-                _puzzleSolver = new PuzzleSolver(
+                var puzzleSolver = new PuzzleSolver(
                     puzzle,
                     OnSolutionFound,
                     OnSearchStep,
                     SynchronizationContext.Current,
                     _cancellationTokenSource.Token);
 
-                _puzzleSolver.SolvePuzzle();
+                puzzleSolver.SolvePuzzle();
             };
 
             Closed += (_, __) => _cancellationTokenSource?.Cancel();
         }
 
-        private void OnSearchStep(IImmutableList<InternalRow> searchStepInternalRows)
+        private void OnTick()
         {
+            if (!_searchSteps.Any()) return;
+
+            var internalRows = _searchSteps.Dequeue();
+
+            if (internalRows == null)
+            {
+                _timer.Stop();
+                return;
+            }
+
+            var newInternalRows = internalRows.Where(x => !x.IsInitialValue).ToImmutableList();
+
+            _currentInternalsRows.Except(newInternalRows, new SameCoordsComparer())
+                .ToList()
+                .ForEach(RemoveInternalRow);
+
+            newInternalRows.Except(_currentInternalsRows, new SameCoordsComparer())
+                .ToList()
+                .ForEach(AddInternalRow);
+
+            newInternalRows.Intersect(_currentInternalsRows, new SameCoordsDifferentValueComparer())
+                .ToList()
+                .ForEach(ChangeInternalRow);
         }
 
-        private void OnSolutionFound(IImmutableList<InternalRow> solutionInternalRows)
+        private void AddInternalRow(InternalRow internalRow)
         {
-            foreach (var x in solutionInternalRows)
+            _bc.AddDigit(internalRow.Coords, internalRow.Value);
+            _currentInternalsRows.Add(internalRow);
+        }
+
+        private void ChangeInternalRow(InternalRow internalRow)
+        {
+            RemoveInternalRow(internalRow);
+            AddInternalRow(internalRow);
+        }
+
+        private void RemoveInternalRow(InternalRow internalRow)
+        {
+            _bc.RemoveDigit(internalRow.Coords);
+            _currentInternalsRows.RemoveAll(x => x.Coords.Equals(internalRow.Coords));
+        }
+
+        private void OnSearchStep(IImmutableList<InternalRow> internalRows)
+        {
+            if (!_timer.IsEnabled) _timer.Start();
+            _searchSteps.Enqueue(internalRows);
+        }
+
+        private void OnSolutionFound(IImmutableList<InternalRow> internalRows)
+        {
+            _searchSteps.Enqueue(null);
+            _cancellationTokenSource = null;
+        }
+
+        private class SameCoordsComparer : IEqualityComparer<InternalRow>
+        {
+            public bool Equals(InternalRow x, InternalRow y)
             {
-                IBoardControl bc = BoardControl;
-                if (!x.IsInitialValue)
-                {
-                    bc.AddDigit(x.Coords, x.Value);
-                }
+                return x.Coords.Equals(y.Coords);
+            }
+
+            public int GetHashCode(InternalRow obj)
+            {
+                return 0;
+            }
+        }
+
+        private class SameCoordsDifferentValueComparer : IEqualityComparer<InternalRow>
+        {
+            public bool Equals(InternalRow x, InternalRow y)
+            {
+                return x.Coords.Equals(y.Coords) && x.Value != y.Value;
+            }
+
+            public int GetHashCode(InternalRow obj)
+            {
+                return 0;
             }
         }
     }
