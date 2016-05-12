0b35e37d2d0e5efc24c11974ee75761df24aacff
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-05-12 18:31:44 +0100

    Fixed failing unit test

--- SudokuDlxWpfTests/MainWindowViewModelTests.cs
+++ SudokuDlxWpfTests/MainWindowViewModelTests.cs
@@ -7,60 +7,65 @@ namespace SudokuDlxWpfTests
     [TestFixture]
     public class MainWindowViewModelTests
     {
+        private Mock<IBoardControl> _boardControlMock;
+        private MainWindowViewModel _vm;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _boardControlMock = new Mock<IBoardControl>();
+            _vm = new MainWindowViewModel(_boardControlMock.Object);
+            _vm.LoadedCommand.Execute(null);
+        }
+
         [Test]
         public void StateOfButtonsInitially()
         {
-            var boardControlMock = new Mock<IBoardControl>();
-            var vm = new MainWindowViewModel(boardControlMock.Object);
-            Assert.That(vm.SolveCommand.CanExecute(null), Is.True);
-            Assert.That(vm.ResetCommand.CanExecute(null), Is.False);
-            Assert.That(vm.CancelCommand.CanExecute(null), Is.False);
+            Assert.That(_vm.SolveCommand.CanExecute(null), Is.True);
+            Assert.That(_vm.ResetCommand.CanExecute(null), Is.False);
+            Assert.That(_vm.CancelCommand.CanExecute(null), Is.False);
         }
 
         [Test]
         public void StateOfButtonsWhenSolving()
         {
-            var boardControlMock = new Mock<IBoardControl>();
-            var vm = new MainWindowViewModel(boardControlMock.Object);
-            vm.SolveCommand.Execute(null);
-            Assert.That(vm.SolveCommand.CanExecute(null), Is.False);
-            Assert.That(vm.ResetCommand.CanExecute(null), Is.False);
-            Assert.That(vm.CancelCommand.CanExecute(null), Is.True);
+            Assert.That(_vm.SolveCommand.CanExecute(null), Is.True);
+            _vm.SolveCommand.Execute(null);
+
+            Assert.That(_vm.SolveCommand.CanExecute(null), Is.False);
+            Assert.That(_vm.ResetCommand.CanExecute(null), Is.False);
+            Assert.That(_vm.CancelCommand.CanExecute(null), Is.True);
         }
 
         [Test]
         public void StateOfButtonsAfterCancellation()
         {
-            var boardControlMock = new Mock<IBoardControl>();
-            var vm = new MainWindowViewModel(boardControlMock.Object);
+            Assert.That(_vm.SolveCommand.CanExecute(null), Is.True);
+            _vm.SolveCommand.Execute(null);
 
-            vm.SolveCommand.Execute(null);
+            Assert.That(_vm.CancelCommand.CanExecute(null), Is.True);
+            _vm.CancelCommand.Execute(null);
 
-            Assert.That(vm.CancelCommand.CanExecute(null), Is.True);
-            vm.CancelCommand.Execute(null);
-
-            Assert.That(vm.SolveCommand.CanExecute(null), Is.True);
-            Assert.That(vm.ResetCommand.CanExecute(null), Is.True);
-            Assert.That(vm.CancelCommand.CanExecute(null), Is.False);
+            Assert.That(_vm.SolveCommand.CanExecute(null), Is.True);
+            Assert.That(_vm.ResetCommand.CanExecute(null), Is.True);
+            Assert.That(_vm.CancelCommand.CanExecute(null), Is.False);
         }
 
         [Test]
         public void StateOfButtonsAfterCancellationThenReset()
         {
-            var boardControlMock = new Mock<IBoardControl>();
-            var vm = new MainWindowViewModel(boardControlMock.Object);
+            Assert.That(_vm.SolveCommand.CanExecute(null), Is.True);
+            _vm.SolveCommand.Execute(null);
 
-            vm.SolveCommand.Execute(null);
+            Assert.That(_vm.CancelCommand.CanExecute(null), Is.True);
+            _vm.CancelCommand.Execute(null);
 
-            Assert.That(vm.CancelCommand.CanExecute(null), Is.True);
-            vm.CancelCommand.Execute(null);
+            Assert.That(_vm.ResetCommand.CanExecute(null), Is.True);
+            _vm.ResetCommand.Execute(null);
 
-            Assert.That(vm.ResetCommand.CanExecute(null), Is.True);
-            vm.ResetCommand.Execute(null);
-
-            Assert.That(vm.SolveCommand.CanExecute(null), Is.True);
-            Assert.That(vm.ResetCommand.CanExecute(null), Is.False);
-            Assert.That(vm.CancelCommand.CanExecute(null), Is.False);
+            Assert.That(_vm.SolveCommand.CanExecute(null), Is.True);
+            Assert.That(_vm.ResetCommand.CanExecute(null), Is.False);
+            Assert.That(_vm.CancelCommand.CanExecute(null), Is.False);
         }
     }
 }
