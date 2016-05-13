abbb8f35f06d95c5eeb093582b07e006d1d83490
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-05-13 21:25:52 +0100

    Mock puzzle solver task factory now implements IPuzzleSolverTask

--- SudokuDlxWpfTests/MockPuzzleSolverTaskFactory.cs
+++ SudokuDlxWpfTests/MockPuzzleSolverTaskFactory.cs
@@ -5,14 +5,29 @@ using SudokuDlxWpf.ViewModel;
 
 namespace SudokuDlxWpfTests
 {
-    internal class MockPuzzleSolverTaskFactory : IPuzzleSolverTaskFactory
+    internal class MockPuzzleSolverTaskFactory : IPuzzleSolverTaskFactory, IPuzzleSolverTask
     {
+        private Action<int, IImmutableList<InternalRow>> _onSolutionFound;
+        private Action<int> _onNoSolutionFound;
+        private Action<int, IImmutableList<InternalRow>> _onSearchStep;
+
         public IPuzzleSolverTask Create(
             Action<int, IImmutableList<InternalRow>> onSolutionFound,
             Action<int> onNoSolutionFound,
             Action<int, IImmutableList<InternalRow>> onSearchStep)
         {
-            return new PuzzleSolverTask(onSolutionFound, onNoSolutionFound, onSearchStep);
+            _onSolutionFound = onSolutionFound;
+            _onNoSolutionFound = onNoSolutionFound;
+            _onSearchStep = onSearchStep;
+            return this;
+        }
+
+        public void Solve(Puzzle puzzle)
+        {
+        }
+
+        public void Cancel()
+        {
         }
     }
 }
